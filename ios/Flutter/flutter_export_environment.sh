29130491b93a0a488dc69f53cd79e234d14c9cad
wahyurudiyan <wahyurudiyansaputr@gmail.com>
2019-10-16 23:31:15 +0700

    First initial commit and push our hackathon flutter code

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Wahyu Rudiyan S\Documents\Projects\android\flutter\fly_high\example"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
---
+++ lib/main.dart
@@ -0,0 +1,220 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart' as mqtt;
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_code_scanner/qr_scanner_overlay_shape.dart';
+
+void main() => runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.amber,
+    ),
+    home: QRViewExample()
+  )
+);
+
+const flash_on = "FLASH ON";
+const flash_off = "FLASH OFF";
+const front_camera = "FRONT CAMERA";
+const back_camera = "BACK CAMERA";
+
+class QRViewExample extends StatefulWidget {
+  const QRViewExample({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _QRViewExampleState();
+}
+
+class _QRViewExampleState extends State<QRViewExample> {
+  var cameraState = 'BACK CAMERA';
+  String qrText = "";
+  String broker = 'test.mosquitto.org';
+  String clientIdentifier = 'android';
+  String topic = 'BOSCH_0101/unlock';
+  int port  = 1883;
+
+  double _temp = 20;
+
+  mqtt.MqttClient client;
+  mqtt.MqttConnectionState connectionState;
+
+  QRViewController controller;
+  StreamSubscription subscription;
+
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  void _subscribeToTopic(String topic) {
+    if (connectionState == mqtt.MqttConnectionState.connected){
+      print('[MQTT Client] Subscibing to ${topic.trim()}');
+      client.subscribe(topic, mqtt.MqttQos.exactlyOnce);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Parcel Box'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+              overlay: QrScannerOverlayShape(
+                borderColor: Colors.amber,
+                borderRadius: 10,
+                borderLength: 30,
+                borderWidth: 30,
+                cutOutSize: 300,
+              ),
+            ),
+            flex: 4,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: _connect,
+          tooltip: 'Connect',
+          child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
+
+  void _connect() async {
+    client = mqtt.MqttClient(broker, '');
+    client.port = port;
+
+    /// A websocket URL must start with ws:// or wss:// or Dart will throw an exception, consult your websocket MQTT broker
+    /// for details.
+    /// To use websockets add the following lines -:
+    /// client.useWebSocket = true;
+    /// client.port = 80;  ( or whatever your WS port is)
+    /// Note do not set the secure flag if you are using wss, the secure flags is for TCP sockets only.
+
+    /// Set logging on if needed, defaults to off
+    client.logging(on: true);
+
+    /// If you intend to use a keep alive value in your connect message that is not the default(60s)
+    /// you must set it here
+    client.keepAlivePeriod = 30;
+
+    /// Add the unsolicited disconnection callback
+    client.onDisconnected = _onDisconnected;
+
+    /// Create a connection message to use or use the default one. The default one sets the
+    /// client identifier, any supplied username/password, the default keepalive interval(60s)
+    /// and clean session, an example of a specific one below.
+    final mqtt.MqttConnectMessage connMess = mqtt.MqttConnectMessage()
+        .withClientIdentifier(clientIdentifier)
+        .startClean() // Non persistent session for testing
+        .keepAliveFor(30)
+        .withWillQos(mqtt.MqttQos.atMostOnce);
+    print('[MQTT client] MQTT client connecting....');
+    client.connectionMessage = connMess;
+
+    /// Connect the client, any errors here are communicated by raising of the appropriate exception. Note
+    /// in some circumstances the broker will just disconnect us, see the spec about this, we however will
+    /// never send malformed messages.
+
+    try {
+      await client.connect();
+    } catch (e) {
+      print(e);
+      _disconnect();
+    }
+
+    /// Check if we are connected
+    if (client.connectionState == mqtt.MqttConnectionState.connected) {
+      print('[MQTT client] connected');
+      setState(() {
+        connectionState = client.connectionState;
+      });
+    } else {
+      print('[MQTT client] ERROR: MQTT client connection failed - '
+          'disconnecting, state is ${client.connectionState}');
+      _disconnect();
+    }
+
+    /// The client has a change notifier object(see the Observable class) which we then listen to to get
+    /// notifications of published updates to each subscribed topic.
+    subscription = client.updates.listen(_onMessage);
+
+    client.published.listen((mqtt.MqttPublishMessage msg){
+      print('EXAMPLE::Published notification:: topic is ${msg.variableHeader.topicName}, with Qos ${msg.header.qos}');
+    });
+
+    final mqtt.MqttClientPayloadBuilder message = mqtt.MqttClientPayloadBuilder();
+    message.addString('${qrText}');
+    print('<<< PUBLISHING >>>');
+    print('${qrText}');
+    client.publishMessage(topic, mqtt.MqttQos.atMostOnce, message.payload);
+
+    _subscribeToTopic(topic);
+  }
+
+  void _disconnect() {
+    print('[MQTT client] _disconnect()');
+    client.disconnect();
+    _onDisconnected();
+  }
+
+  void _onDisconnected() {
+    print('[MQTT client] _onDisconnected');
+    setState(() {
+      //topics.clear();
+      connectionState = client.connectionState;
+      client = null;
+      subscription.cancel();
+      subscription = null;
+    });
+    print('[MQTT client] MQTT client disconnected');
+  }
+
+  void _onMessage(List<mqtt.MqttReceivedMessage> event) {
+    print(event.length);
+    final mqtt.MqttPublishMessage recMess = event[0].payload as mqtt.MqttPublishMessage;
+    final String message = mqtt.MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+    /// The above may seem a little convoluted for users only interested in the
+    /// payload, some users however may be interested in the received publish message,
+    /// lets not constrain ourselves yet until the package has been in the wild
+    /// for a while.
+    /// The payload is a byte buffer, this will be specific to the topic
+    print('[MQTT client] MQTT message: topic is <${event[0].topic}>, '
+        'payload is <-- ${message} -->');
+    print(client.connectionState);
+    print("[MQTT client] message with topic: ${event[0].topic}");
+    print("[MQTT client] message with message: ${message}");
+    setState(() {
+      _temp = double.parse(message);
+    });
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        qrText = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
